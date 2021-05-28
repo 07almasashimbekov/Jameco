6f0e191c5abb2367a80665a9b40047c851aca547
07almasashimbekov <75465336+07almasashimbekov@users.noreply.github.com>
2021-05-28 14:20:18 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+const burgerMenu = document.querySelector('.nav_burger'); //выбрать где именно нарисован бургер и скрыт.
+const menuList = document.querySelector('.nav_list'); //родительский элемент листа меню
+const body = document.querySelector('body'); //это просто body для фиксации
+const links = document.querySelectorAll('nav_links'); // ссылки меню
+
+menuList.onclick = function hideMenu() {
+    burgerMenu.classList.toggle('activeBurger');
+    menuList.classList.toggle('activeBurger');
+    body.classList.remove('lockScroll');
+}
+burgerMenu.onclick = function showBurger() {
+    this.classList.toggle('activeBurger');
+    menuList.classList.toggle('activeBurger');
+    body.classList.toggle('lockScroll');
+}
+
+let myBtn = document.querySelector(".btn");
+let drop = document.getElementById("dropdown");
+
+myBtn.addEventListener('click', () => {
+    drop.classList.toggle("show");
+
+})
+
